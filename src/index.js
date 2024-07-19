c5d0cf1f10b9c9c4d9cb39f33a5c3a5331a6c964
kmssss <rlaalstkd177@gmail.com>
2024-07-19 16:33:44 +0900

    register update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import store from './store.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
+  <Provider store={store} >
     <App />
+  </Provider>
   //</React.StrictMode>
 );
 
