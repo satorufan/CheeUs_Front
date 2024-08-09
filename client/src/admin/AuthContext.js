fdebaaa771e822221dad787f9a52f0009b875291
kmssss <rlaalstkd177@gmail.com>
2024-08-09 09:36:27 +0900

    tmp

--- client/src/admin/AuthContext.js
+++ client/src/admin/AuthContext.js
@@ -5,7 +5,7 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     const login = () => setIsLoggedIn(true);
     const logout = () => setIsLoggedIn(false);
