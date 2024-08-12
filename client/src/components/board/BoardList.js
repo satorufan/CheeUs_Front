73523b27b673e9dbf9f514411161aa98bc02ea2e
gyuri <rbfl7645@naver.com>
2024-08-13 01:40:04 +0900

    보드리스트 컴포넌트화

--- client/src/components/board/BoardList.js
+++ client/src/components/board/BoardList.js
@@ -10,24 +10,21 @@ import Favorite from '@mui/icons-material/Favorite';
 import Visibility from '@mui/icons-material/Visibility';
 import PushPinIcon from '@mui/icons-material/PushPin';
 import Chip from '@mui/material/Chip';
-import BoardTop from '../board/BoardTop';
-import {selectBoards, toggleLike, selectLikedMap, filterBoards, setSearchQuery, selectFilteredBoards, fetchBoards, selectBoardAuthors, fetchBoardsAuthor} from '../../store/BoardSlice';
 import Pagination from '@mui/material/Pagination';
-import './freeBoard.css';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import BoardSkeleton from '../skeleton/BoardSkeleton';
-
-const FreeBoard = () => {
+import BoardTop from '../board/BoardTop';
+import { selectBoards, toggleLike, selectLikedMap, filterBoards, setSearchQuery, selectFilteredBoards, fetchBoards, selectBoardAuthors, fetchBoardsAuthor } from '../../store/BoardSlice';
+import '../freeboard/freeBoard.css';
+const BoardList = ({ category }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
   
   const boards = useSelector(selectBoards);
   const authors = useSelector(selectBoardAuthors);
-  const [isLoaded, setIsLoaded] = useState(false);
-  console.log(authors);
   const likedMap = useSelector(selectLikedMap);
   const filteredBoards = useSelector(selectFilteredBoards);
   
@@ -36,8 +33,14 @@ const FreeBoard = () => {
 
   // 보드 목록 로딩
   useEffect(() => {
-      dispatch(fetchBoards('freeboard'));
-  }, [dispatch]);
+    if(category == 1){
+        dispatch(fetchBoards('freeboard'));
+    } else if (category == 3){
+        dispatch(fetchBoards('eventboard'));
+    } else if (category == 2){
+        dispatch(fetchBoards('shortform'));
+    }
+  }, [dispatch, category]);
 
   // URL 쿼리에서 검색어를 읽어와 상태에 설정
   useEffect(() => {
@@ -47,30 +50,38 @@ const FreeBoard = () => {
     dispatch(filterBoards()); // 검색어에 따라 필터링
   }, [location.search, dispatch]);
 
-  useEffect(() => {
-    console.log('Filtered Boards:', filteredBoards);
-  }, [filteredBoards]);
-
   const handleLikeClick = (id) => {
     dispatch(toggleLike(id));
   };
 
   const handleCardClick = (id) => {
-    navigate(`/board/freeboard/detail/${id}`);
+    if(category == 1){
+        navigate(`/board/freeboard/detail/${id}`);
+    } else if (category == 3){
+        navigate(`/board/eventboard/detail/${id}`);
+    } else if (category == 2){
+        navigate(`/board/shortform/detail/${id}`);
+    }
   };
 
   const handleCreatePost = () => {
-    navigate('/board/freeboard/write');
+    if(category == 1) {
+        navigate(`/board/freeboard/write`);
+    } else if(category == 3) {
+        navigate(`/board/eventboard/write`);
+    } else if(category == 2) {
+        navigate(`/board/shortform/write`);
+    }
   };
 
   // 페이지네이션
   const startIndex = (currentPage - 1) * itemsPerPage;
   const visibleBoards = filteredBoards.filter(board => !board.hidden);
-  const pinnedBoards = visibleBoards.filter(board => board.category === 1 && board.pinned);
-  const regularBoards = visibleBoards.filter(board => board.category === 1 && !board.pinned);
+  const pinnedBoards = visibleBoards.filter(board => board.category === category && board.pinned);
+  const regularBoards = visibleBoards.filter(board => board.category === category && !board.pinned);
   const currentBoards = [...pinnedBoards, ...regularBoards].slice(startIndex, startIndex + itemsPerPage);
-  const totalPages = Math.ceil(visibleBoards.filter(board => board.category === 1).length / itemsPerPage);
-  
+  const totalPages = Math.ceil(visibleBoards.filter(board => board.category === category).length / itemsPerPage);
+
   function arraysEqualAsSets(arr1, arr2) {
     return new Set(arr1).size === new Set([...arr1, ...arr2]).size;
   }
@@ -82,9 +93,9 @@ const FreeBoard = () => {
 
   useEffect(() => {
     if (currentBoards.length > 0 && !arraysEqualAsSets(Object.keys(authors), perPageAuthors)) {
-      dispatch(fetchBoardsAuthor({category: 'freeboard', perPageBoards: currentBoards}));
+      dispatch(fetchBoardsAuthor({ category, perPageBoards: currentBoards }));
     }
-  }, [dispatch, currentBoards, boards]);
+  }, [dispatch, currentBoards, boards, category]);
 
   const handleChange = (event, value) => {
     setCurrentPage(value);
@@ -107,8 +118,8 @@ const FreeBoard = () => {
                   <PushPinIcon className="pinned-icon" />
                   <Chip label="공지" size="small" className="notice-chip" />
                 </Box>
-              )}            
-              <Box className="card-video free">
+              )}
+              <Box className="card-video">
                 <AspectRatio ratio="4/3">
                   {board.photoes ? (
                     <CardCover className="card-cover">
@@ -122,11 +133,11 @@ const FreeBoard = () => {
                       </div>
                     </CardCover>
                   ) : (
-                    <div className="content-text">
-                      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
-                        {board.content}
-                      </ReactMarkdown>
-                    </div>
+                      <div className="content-text">
+                       <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+                          {board.content}
+                        </ReactMarkdown>
+                      </div>
                   )}
                 </AspectRatio>
               </Box>
@@ -196,4 +207,4 @@ const FreeBoard = () => {
   );
 };
 
-export default FreeBoard;
+export default BoardList;
